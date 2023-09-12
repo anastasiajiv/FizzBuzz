15736d633a423036383dbc89b940cc932791acb8
Anastasia <anastasiajiv13@gmail.com>
2023-09-11 23:45:46 -0400

    refactored to extract helper method

---
+++ src/Multiplies.java
@@ -0,0 +1,15 @@
+public class Multiplies {
+    public static void main(String[] args) {
+        int multiplies = 0;
+        for (int i = 0; i < 1000; i++){
+            boolean multiple_of_3 = i % 3 == 0;
+            boolean multiple_of_5 = i % 5 == 0;
+
+            if (multiple_of_3 || multiple_of_5) {
+                multiplies+=1;
+            }
+
+        }
+        System.out.println(multiplies);
+    }
+}
