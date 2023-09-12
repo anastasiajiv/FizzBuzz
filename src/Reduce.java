ba35b46e2650ee5c1351d8594a31f004774c3368
Anastasia <anastasiajiv13@gmail.com>
2023-09-11 23:46:24 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int counts = 0;
+        int value = 100;
+        while(value != 0){
+            if (value % 2 == 0){
+                value = value / 2;
+                counts++;
+            }else {
+                value = value - 1;
+                counts++;
+            }
+        }
+        System.out.println(counts);
+    }
 }
